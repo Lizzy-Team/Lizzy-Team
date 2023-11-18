89aeee6d0d1b6bd5d4be5c9ee68c850cd3292409
arylinggarp <arylinggar899@gmail.com>
2023-11-18 22:03:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/remember_lizzy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.remember_lizzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
