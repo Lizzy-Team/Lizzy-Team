7e9036eaf635dcf178f233983ef9882d8c2a8e83
FRANCISCUS XAVERIUS ANDIKA HARDITO <202165016@student.unipa.ac.id>
2023-11-21 00:57:44 +0900

    update 1.0.1

---
+++ app/src/main/java/com/example/remember_lizzy/kolaborasi/KolaborasiActivity.kt
@@ -0,0 +1,13 @@
+package com.example.remember_lizzy.kolaborasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.remember_lizzy.R
+
+class KolaborasiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kolaborasi)
+    }
+}
