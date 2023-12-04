e3af0d433ef4565ae05304e06fefb21a5660bb9d
arylinggarp <arylinggar899@gmail.com>
2023-12-05 04:23:32 +0700

    onboarding dll, bismilah

---
+++ app/src/main/java/com/example/remember_lizzy/EditProfilActivity.kt
@@ -0,0 +1,11 @@
+package com.example.remember_lizzy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profil)
+    }
+}
