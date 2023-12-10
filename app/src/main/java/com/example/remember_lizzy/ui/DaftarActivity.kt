b3deee8583f2874caa77ee5773b25f85f2771a2e
arylinggarp <arylinggar899@gmail.com>
2023-12-10 18:55:46 +0700

    tambah screen daftar

---
+++ app/src/main/java/com/example/remember_lizzy/ui/DaftarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.remember_lizzy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.remember_lizzy.R
+
+class DaftarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar)
+    }
+}
--- app/src/main/java/com/example/remember_lizzy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/remember_lizzy/ui/profile/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.remember_lizzy.EditProfilActivity
 import com.example.remember_lizzy.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,27 +36,18 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val textViewEditProfile: TextView = view.findViewById(R.id.textView4)
+
+        // Tambahkan listener klik pada textViewEditProfile
+        textViewEditProfile.setOnClickListener {
+            // Gantilah MyOtherFragment dengan fragment tujuan yang ingin Anda tampilkan
+            val myOtherFragment = EditProfilActivity()
+
+
+        }
+
+        return view
     }
 }
